02cc48913f6db300bf7ad313c8add8908b0c9c9f
Tania María Sánchez Irola <84118313+TaniaMariaSanchezIrola@users.noreply.github.com>
2024-02-15 11:29:18 -0600

    Update streamlit_app.py
    add breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ streamlit.title('My Parents New Healty Dinner')
 
 streamlit.header('Dinner')
 streamlit.text('Cereal')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
