ec7c4f88cb33dc199ff35a50b0a1602b9531a837
Tania María Sánchez Irola <84118313+TaniaMariaSanchezIrola@users.noreply.github.com>
2024-02-20 16:17:02 -0600

    Update streamlit_app.py
    kk

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
   # take the json version of the response and normalize it 
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruitvyce_normalized
+  return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
